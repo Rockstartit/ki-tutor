ffec8719fb51084cff29a27973f975549b2535f3
Luca Huettner <luca.huettner@gmail.com>
2024-07-16 14:59:29 +0200

    feat: Add create_thread endpoint

---
+++ main.py
@@ -0,0 +1,61 @@
+from typing import Optional
+
+from dotenv import load_dotenv
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from openai import AsyncOpenAI
+from openai.types.beta.threads.run import RequiredAction, LastError
+
+from pydantic import BaseModel
+
+load_dotenv()
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",  # used to run with react server
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+client = AsyncOpenAI()
+assistant_id = "asst_nUCtNHJFFdODxsSde7TJ0iJ2" #id of the RockStartIt Assistant
+
+class RunStatus(BaseModel):
+    run_id: str
+    thread_id: str
+    status: str
+    required_action: Optional[RequiredAction]
+    last_error: Optional[LastError]
+
+@app.post("/create_thread")
+async def create_thread():
+    thread = await client.beta.threads.create()
+
+    await client.beta.threads.messages.create(
+        thread_id=thread.id,
+        content="Greet the user and tell it about yourself and ask it what it is looking for.",
+        role="user",
+        metadata={
+            "type": "hidden"
+        }
+    )
+
+    run = await client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant_id
+    )
+
+    return RunStatus(
+        run_id=run.id,
+        thread_id=thread.id,
+        status=run.status,
+        required_action=run.required_action,
+        last_error=run.last_error
+    )
