4ace0ab1b5ac43554d9aa4b5240c7f0b09f1e561
Luca Huettner <luca.huettner@gmail.com>
2024-08-19 16:18:20 +0200

    feat: Add organization id to the openai api requests

--- ki-tutor-app/app/openai.ts
+++ ki-tutor-app/app/openai.ts
@@ -1,5 +1,7 @@
 import OpenAI from "openai";
 
 //api key
-export const openai = new OpenAI();
+export const openai = new OpenAI({
+    organization: "org-JOZZVGILnxuXT7NdjxlKF7g8"
+});
 export const assistantId = "asst_nUCtNHJFFdODxsSde7TJ0iJ2"; // set your assistant ID here
